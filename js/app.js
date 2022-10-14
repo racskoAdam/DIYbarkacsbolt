c49e36c82bf6aa4f16eb5894ae461d803fc90a0f
Racskó Ádám Ákos <racsko.adam@keri.local>
2022-10-14 13:16:54 +0200

    szerszamok/faanyagok js

--- js/app.js
+++ js/app.js
@@ -34,35 +34,81 @@ const data ={
 
     "szerszamok":[
         {
-
-        }
+            "id": 0,
+            "nev": "ASD furó",
+            "keplink":"../media/img/szerszamok/baltak-min.png",
+            "ar": "4500 Ft"
+        },
+        {
+            "id": 0,
+            "nev": "ASD furó",
+            "keplink":"../media/img/szerszamok/baltak-min.png",
+            "ar": "4500 Ft"
+        },
+        {
+            "id": 0,
+            "nev": "ASD furó",
+            "keplink":"../media/img/szerszamok/baltak-min.png",
+            "ar": "4500 Ft"
+        },
+        {
+            "id": 0,
+            "nev": "ASD furó",
+            "keplink":"../media/img/szerszamok/baltak-min.png",
+            "ar": "4500 Ft"
+        },
+        {
+            "id": 0,
+            "nev": "ASD furó",
+            "keplink":"../media/img/szerszamok/baltak-min.png",
+            "ar": "4500 Ft"
+        },
     ]
 }
 
-let container = document.getElementById("items");
+var path = window.location.pathname;
+var page = path.split("/").pop();
+let adat;
+switch (page) {
+    case "faanyagok.html":
+        adat = data.faanyagok;
+        load();
+        break;
+        case "szerszamok.html":
+        console.log("szerszamok");
+        adat = data.szerszamok;
+        load();
+            break;
+    default:
+        break;
+}
 
-let row = document.createElement("div");
-row.classList.add("row");
-data.faanyagok.forEach(item => {
-    let name = item.nev;
-    let image = item.keplink;
-    let price = item.ar;
-            //kártya meghatározása
-            let card =`
-            <div class="card aru">
-              <img src="%IMG%" class="card-img-top" alt="image">
-              <div class="card-body">
-                <p class="card-title">%NAME%</p>
-                <p class="card-text">%PRICE%</p>
-              </div>
-            </div>
-            `;
-    card = card.replace("%NAME%",name); //%NAME%" kicserélése a cardban name változóra
-    card = card.replace("%IMG%",image); //%IMG%" kicserélése a cardban image változóra
-    card = card.replace("%PRICE%",price); //%IMG%" kicserélése a cardban image változóra
-    let asd = document.createElement("div");
-    asd.classList.add("col-md-3");
-    asd.innerHTML = card;
-    row.append(asd)
-    container.append(row);
-});
+function load(){
+    let container = document.getElementById("items");
+
+    let row = document.createElement("div");
+    row.classList.add("row");
+    adat.forEach(item => {
+        let name = item.nev;
+        let image = item.keplink;
+        let price = item.ar;
+                //kártya meghatározása
+                let card =`
+                <div class="card aru">
+                  <img src="%IMG%" class="card-img-top" alt="image">
+                  <div class="card-body">
+                    <p class="card-title">%NAME%</p>
+                    <p class="card-text">%PRICE%</p>
+                  </div>
+                </div>
+                `;
+        card = card.replace("%NAME%",name); //%NAME%" kicserélése a cardban name változóra
+        card = card.replace("%IMG%",image); //%IMG%" kicserélése a cardban image változóra
+        card = card.replace("%PRICE%",price); //%IMG%" kicserélése a cardban image változóra
+        let asd = document.createElement("div");
+        asd.classList.add("col-md-3");
+        asd.innerHTML = card;
+        row.append(asd)
+        container.append(row);
+    });
+}
