940bb539c231dda96826f0233d08888c0c96fdaa
Konyhasneni22 <balla.istvan-2018@keri.mako.hu>
2022-10-14 20:55:22 +0200

    faszgeci

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//TERMÉKADATOK
 const data ={
     "faanyagok":[
         {
@@ -65,7 +66,7 @@ const data ={
         },
     ]
 }
-
+//TERMÉKADATOK MEGHÍVÁSA
 var path = window.location.pathname;
 var page = path.split("/").pop();
 let adat;
@@ -112,4 +113,85 @@ function load(){
         row.append(asd)
         container.append(row);
     });
+}
+
+
+
+
+
+//FESTÉKEK
+let szoveg=document.getElementById("szoveg")
+let kep=document.getElementById("kep");
+var x = document.getElementById("d1id");
+function d1(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="1.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg"
+}
+function d2(){                                                                               
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="2.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d3(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="3.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d4(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="4.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d5(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="5.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d6(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="6.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d7(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="7.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d8(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="8.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function d9(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    kep.src="9.jpg";
+    szoveg.innerHTML="igfuhuidrtfhbuyg2"
+}
+function bezar(){
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    kep.src="";
+    szoveg.innerHTML=""
 }
